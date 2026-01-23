f96e7d38771e1dae4d1d677b3f46ef3541f9bb02
zhigarmosta <zhigarkirill@gmail.com>
2026-01-23 17:21:15 +0500

    init

---
+++ app/app.vue
@@ -0,0 +1,22 @@
+  <template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+
+<script setup lang="ts">
+// Делаем запрос при загрузке страницы
+const { data, error } = await useFetch('http://127.0.0.1:8000/volt12/hello')
+
+// Если пришло, выводим в консоль браузера
+if (data.value) {
+  console.log('Ответ от Symfony:', data.value)
+}
+
+// Если ошибка - тоже выводим
+if (error.value) {
+  console.error('Ошибка при запросе:', error.value)
+}
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+export default defineNuxtConfig({
+  runtimeConfig: {
+    public: { apiBase: 'http://127.0.0.1:8000' }
+  }
+})
